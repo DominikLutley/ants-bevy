845c043cda00a25cc288f561adc13fedad99a81c
Dominik Lutley <domlutley@gmail.com>
2023-02-19 11:51:16 +0100

    Build in root

--- build-wasm.sh
+++ build-wasm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cargo build --release --target wasm32-unknown-unknown
-wasm-bindgen --out-dir ./docs/out/ --target web ./target/wasm32-unknown-unknown/release/ants-bevy.wasm
+wasm-bindgen --out-dir ./out/ --target web ./target/wasm32-unknown-unknown/release/ants-bevy.wasm
